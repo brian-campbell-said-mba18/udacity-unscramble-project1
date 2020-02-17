595e2847700b3f11113196af4b58dd2c48d8f14c
brian <bgcapbell@email.wm.edu>
2020-02-17 06:40:32 -0600

    Task 2, in progress part 2

--- Task2.py
+++ Task2.py
@@ -22,4 +22,23 @@ September 2016.".
 # First, this converts the time data from strings to integers.
 for i in range(len(calls)):
     calls[i][3] = int(calls[i][3])
-# Second, 
+
+# Second, we create the call dictionary and append the numbers
+# that originated the calls and time spent on calls from the calls list.
+call_dict = {}
+for i in calls:
+    if i[0] not in call_dict:
+        call_dict[i[0]] = i[3]
+    else:
+        call_dict[i[0]] += i[3]
+
+# Third, we add the times to the numbers that received the calls.
+for i in calls:
+    if i[1] not in call_dict:
+        call_dict[i[1]] = i[3]
+    else:
+        call_dict[i[1]] += i[3]
+
+# This tests whether the call_dict was successfully created.
+for keys, values in call_dict.items():
+    print("Phone Numbers: {}, Times(seconds) : {}.".format(keys, values))
