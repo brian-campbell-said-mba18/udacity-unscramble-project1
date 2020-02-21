db17e6d04645f9c03ad14bbccf250d16d05a7acc
brian <bgcapbell@email.wm.edu>
2020-02-21 17:41:25 -0600

    completed Task4, first pass

--- Task4.py
+++ Task4.py
@@ -38,18 +38,48 @@ for i in calls:
     if i[1] not in receive_list:
         receive_list.append(i[1])
 
-# This for loop creates two lists. If a number only calls,
-# but never receives calls, it is put into the suspects_list,
-# as a potential scammer. However, if a number both calls and
-# receeives calls, it passes the test and is put into the
-# passed_inbound_calls list.
-suspects_list = []
-passed_inbound_calls = []
+# Third, this for loop creates two lists. If a number only calls,
+# but never receives calls, it is put into a suspects list,
+# failed_income_call_test. 
+failed_income_call_test = []
 for i in og_list:
     if i not in receive_list:
-        suspects_list.append(i)
-    else:
-        passed_inbound_calls.append(i)
+        failed_income_call_test.append(i)
 
+# Fourth, I create a list of unique phone numbers that send texts.
+text_senders = []
+for i in texts:
+    if i[0] not in text_senders:
+        text_senders.append(i[0])
 
+# Fifth, I create a test to determine of the phone numbers
+# that failed the first incoming call test, which of those numbers
+# also fail the text-sending test. I append these numbers to the list,
+# failed_text_send_test.
+failed_text_send_test = []
+for i in failed_income_call_test:
+    if i not in text_senders:
+        failed_text_send_test.append(i)
+
+# Sixth, I create a list of unique phone numbers that receive texts.
+text_receivers = []
+for i in texts:
+    if i[1] not in text_receivers:
+        text_receivers.append(i[1])
+
+# Finally, I create a test to determine of the phone numbers that
+# failed the text-senders test, which of these numbers fail the final
+# test, the text-receiving tests. The numbers that fail are put into
+# the list, flagged_suspects. I also sort the numbers in ascending order
+# in flagged_suspects.
+flagged_suspects = []
+for i in failed_text_send_test:
+    if i not in text_receivers:
+        flagged_suspects.append(i)
+flagged_suspects = sorted(flagged_suspects)
+
+# This prints the results.
+print("These numbers could be telemarketers: ")
+for i in flagged_suspects:
+    print(i)
 
